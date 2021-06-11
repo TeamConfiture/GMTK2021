a24273b712fb8b7b3a33d50ffbd4d9da59a76add
Kimi <tran_kim-anh97@hotmail.fr>
2021-06-11 23:34:15 +0200

    feat : Add menu

---
+++ New Unity Project/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+
+    public void PlayNowButton(string scene) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+    public void CreditsButton() {
+        CreditsMenu.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void MainMenuButton() {
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+    public void BackButton(){
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+    public void QuitButton() {
+        Application.Quit();
+    }
+    
+}
