cc1c09a2b1c12bf1066d0480041eae21f7ed4706
Kimi <tran_kim-anh97@hotmail.fr>
2021-06-13 15:15:55 +0200

    feat: add music

--- New Unity Project/Assets/Scripts/CupidonScript.cs
+++ New Unity Project/Assets/Scripts/CupidonScript.cs
@@ -6,16 +6,19 @@ public class CupidonScript : MonoBehaviour
 {
 
     public Animator anim;
+    private AudioSource fleche;
 
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        fleche = GetComponent<AudioSource>();
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             anim.Play("Cupidon");
+            fleche.Play();
         }
     }
 }
